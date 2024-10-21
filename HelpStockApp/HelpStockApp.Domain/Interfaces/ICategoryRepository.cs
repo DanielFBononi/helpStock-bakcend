ef7e413130fc76f0a6fe0695125691a4d77897e8
DanielFBononi <daniel.bononi@fatec.sp.gov.br>
2024-10-21 10:25:06 -0300

    Aula1021- Interfaces de Domain

---
+++ HelpStockApp/HelpStockApp.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using HelpStockApp.Domain.Entities;
+
+namespace HelpStockApp.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetById(int? id);
+        Task<Category> CreateCategory(Category category);
+        Task<Category> UpdateCategory(Category category);
+        Task<Category> RemoveCategory(Category category);
+    }
+}
---
+++ HelpStockApp/HelpStockApp.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using HelpStockApp.Domain.Entities;
+
+namespace HelpStockApp.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetById(int? id);
+        Task<Product> CreateProduc(Product product);
+        Task<Product> UpdateProduc(Product product);
+        Task<Product> RemoveProduct(Product product);
+    }
+}
